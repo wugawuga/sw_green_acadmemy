8018b8a9ea60c582d7eb194c7aa058b5e5efe88f
Wugawuga <dnr3655@naver.com>
2021-12-11 23:09:27 +0900

    변경 - java 문제 해결

--- java/project/src/project/Ques01.java
+++ java/project/src/project/Ques01.java
@@ -6,8 +6,9 @@ public class Ques01 {
 	
 	static Scanner scan = new Scanner(System.in);
 	
-	static String[] names = new String[5]; // 학생이름 저장공간
-	static int[][] scores = new int[5][4]; // 학생성적 저장공간
+	static String[] names = {"", "", "", "", ""}; // 학생이름 저장공간
+	static int[][] scores = new int[5][3]; // 학생성적 저장공간
+	static String[] subjects = new String[] {"영어","국어","수학"};
 	static double[]	average = new double[5];// 학생평균 저장공간
 	static int[] grade = new int[] {1,1,1,1,1}; // 학생석차 저장공간
 	
@@ -52,17 +53,40 @@ public class Ques01 {
 
 	private static void selectScore() { // 성적조회
 		
+		boolean stop = false;
+		Loop:
+		      while(!stop) {
+		         System.out.print("이름을 입력하세요 나가시려면 n을 입력하세요 : ");
+		         String ckname = scan.nextLine();
+		         String no = "n";
+		         
+		         for(int i = 0; i < names.length; i++) {
+		            if(names[i].equals(ckname)){
+		               for(int j = 0; j < scores[i].length; j++) {
+		                  System.out.println(subjects[j]+" : "+scores[i][j]);
+		               }
+		               continue Loop;               
+		            }
+		         }
+		         if(no.equals(ckname)) {
+		            return;
+		         }
+		         System.out.println("다시 입력하세요");
+		      }
+		
 	}
 
 	private static void inputScore() { // 성적입력
 		for(int i = 0; i < names.length; i++) {
+			int sum = 0;
 			System.out.print("이름을 입력하세요. : ");
 			names[i] = scan.nextLine();
 			for(int j = 0; j < 3; j++) {
-				System.out.println("성적을 입력하세요.");
-				System.out.print("국어, 영어, 수학 : ");
+				System.out.print(subjects[j]+"점수를 입력하세요 : ");
 				scores[i][j] = Integer.parseInt(scan.nextLine());
+				sum += scores[i][j];
 			}
+			average[i] = sum/scores[i].length;
 		}
 	}
 
